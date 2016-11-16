39ede14ef98add7395473464b5c953007b373bdd
lukas mzyk <lukas mzyk>
2016-11-16 17:18:17 +0100

    Add try-except statement
    add deleted screenshots
    
    add deleted screenshots
    
    Try except statement modified
    
    change file name in path

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ from stop_words import get_stop_words
 
 def getWordList(url):
 	word_list = []
-	
+
 	source_code = requests.get(url)
 	plain_text = source_code.text
 	soup = BeautifulSoup(plain_text,'lxml')
-	
+
 	for text in soup.findAll('p'):
 		if text.text is None:
 			continue
@@ -58,14 +58,13 @@ def createFrquencyTable(word_list):
 
 def remove_stop_words(frequency_list):
 	stop_words = get_stop_words('en')
-	
+
 	temp_list = []
 	for key,value in frequency_list:
 		if key not in stop_words:
 			temp_list.append([key, value])
 
 	return temp_list
-	
 
 
 
@@ -83,34 +82,38 @@ if(len(sys.argv) > 2):
 else:
 	search_mode = False
 
+
 url = wikipedia_api_link + string_query
-response = requests.get(url)
+try:
+    response = requests.get(url)
 
-data = json.loads(response.content)
-wikipedia_page_tag = data['query']['search'][0]['title']
+    data = json.loads(response.content)
+    wikipedia_page_tag = data['query']['search'][0]['title']
 
-url = wikipedia_link + wikipedia_page_tag
-page_word_list = getWordList(url)
-page_word_count = createFrquencyTable(page_word_list)
+    url = wikipedia_link + wikipedia_page_tag
+    page_word_list = getWordList(url)
+    page_word_count = createFrquencyTable(page_word_list)
 
-sorted_word_frequency_list = sorted(page_word_count.items(), key=operator.itemgetter(1), reverse=True)
+    sorted_word_frequency_list = sorted(page_word_count.items(), key=operator.itemgetter(1), reverse=True)
 
-if(search_mode):
-	sorted_word_frequency_list = remove_stop_words(sorted_word_frequency_list)
+    if(search_mode):
+        sorted_word_frequency_list = remove_stop_words(sorted_word_frequency_list)
 
-total_words_sum = 0
-for key,value in sorted_word_frequency_list:
-	total_words_sum = total_words_sum + value
+    total_words_sum = 0
+    for key,value in sorted_word_frequency_list:
+        total_words_sum = total_words_sum + value
 
-if len(sorted_word_frequency_list) > 20:
-	sorted_word_frequency_list = sorted_word_frequency_list[:20]
+    if len(sorted_word_frequency_list) > 20:
+        sorted_word_frequency_list = sorted_word_frequency_list[:20]
 
-final_list = []
-for key,value in sorted_word_frequency_list:
-	percentage_value = float(value * 100) / total_words_sum
-	final_list.append([key, value, round(percentage_value, 4)])
+    final_list = []
+    for key,value in sorted_word_frequency_list:
+        percentage_value = float(value * 100) / total_words_sum
+        final_list.append([key, value, round(percentage_value, 4)])
 
-print_headers = ['Word', 'Frequency', 'Frequency Percentage']
+    print_headers = ['Word', 'Frequency', 'Frequency Percentage']
 
-print tabulate(final_list, headers=print_headers, tablefmt='orgtbl')
+    print tabulate(final_list, headers=print_headers, tablefmt='orgtbl')
 
+except Exception, e:
+    print "Error :", e
